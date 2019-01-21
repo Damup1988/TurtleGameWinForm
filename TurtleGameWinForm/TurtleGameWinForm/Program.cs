540c0956542a78e1af4573e8c5bb8134409123ba
SafarovD <damup1988@gmail.com>
2019-01-22 01:32:21 +0300

    New project - winform game Turtle

---
+++ TurtleGameWinForm/TurtleGameWinForm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurtleGameWinForm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
