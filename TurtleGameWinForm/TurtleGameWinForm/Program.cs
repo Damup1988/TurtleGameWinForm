e7a549179cbc3b8808d809c99290154e8529558f
SafarovD <damup1988@gmail.com>
2019-01-27 13:30:30 +0300

    I've added class Food. Unfortunately it doesn't work properly :( When the turtle hits the food a method creates new one on the field, but the previous food doesn't disappear. Don't know how it should be done.

--- TurtleGameWinForm/TurtleGameWinForm/Program.cs
+++ TurtleGameWinForm/TurtleGameWinForm/Program.cs
@@ -12,13 +12,45 @@ namespace TurtleGameWinForm
     {
         static void Main(string[] args)
         {
-            Turtle.Speed = 9;
-            int i = 0;
-            while (i < 4)
+            GraphicsWindow.KeyDown += GraphicsWindow_KeyDown;
+            Turtle.PenUp();
+
+            Food apple = new Food(200,200,10,10);            
+
+            apple.CreateFood();
+
+            Random r = new Random();
+
+            while (true)
             {
-                Turtle.Move(100);
-                Turtle.TurnRight();
-                i++;
+                Turtle.Move(10);
+                if (Turtle.X >= apple.x && Turtle.X <= apple.x + 10 && Turtle.Y >= apple.y && Turtle.Y <= apple.y + 10)
+                {
+                    apple.x = r.Next(0, GraphicsWindow.Width);
+                    apple.y = r.Next(0, GraphicsWindow.Height);
+                    apple.CreateFood();
+                }
+                
+            }
+        }
+
+        private static void GraphicsWindow_KeyDown()
+        {
+            if(GraphicsWindow.LastKey == "Up")
+            {
+                Turtle.Angle = 0;
+            }
+            else if(GraphicsWindow.LastKey == "Right")
+            {
+                Turtle.Angle = 90;
+            }
+            else if(GraphicsWindow.LastKey == "Down")
+            {
+                Turtle.Angle = 180;
+            }
+            else if(GraphicsWindow.LastKey == "Left")
+            {
+                Turtle.Angle = 270;
             }
         }
     }
---
+++ TurtleGameWinForm/TurtleGameWinForm/food.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace TurtleGameWinForm
+{
+    class Food
+    {
+        public int x, y;
+        public int size_a, size_b;
+        
+
+        public Food(int x, int y, int size_a, int size_b)
+        {
+            this.x = x;
+            this.y = y;
+            this.size_a = size_a;
+            this.size_b = size_b;
+        }
+
+        public void CreateFood()
+        {
+            //var food = ;
+            //Shapes.Remove(food);
+            var food = Shapes.AddRectangle(size_a,size_b);
+            Shapes.Move(food,x,y);
+        }
+    }
+}
